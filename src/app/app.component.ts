58a7c548c67fc68deb763985ccf34350ccb22a72
BryanLanghendries <bryan.langhendries@student.ehb.be>
2025-01-31 20:30:09 +0100

    small update to imports and spacing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { SidenavComponent } from './shared/sidenav/sidenav.component';
-import { TopnavComponent } from './shared/topnav/topnav.component';
+import { SidenavComponent } from './core/sidenav/sidenav.component';
+import { TopnavComponent } from './core/topnav/topnav.component';
 import { LMarkdownEditorModule } from 'ngx-markdown-editor';
 
-import { ProjectOverviewComponent } from './feature/project-overview/project-overview.component';
+
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, SidenavComponent, TopnavComponent,FormsModule, LMarkdownEditorModule],
+  imports: [RouterOutlet, SidenavComponent, TopnavComponent, FormsModule, LMarkdownEditorModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
