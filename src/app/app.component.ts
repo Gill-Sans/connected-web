bafb497eb6db4a13d6791e37b65417546c3ec69d
Lucaswillaert <lucas.willaert@student.ehb.be>
2025-01-31 09:42:55 +0100

    add project creation feature with form and markdown editor; update routing and styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SidenavComponent } from './shared/sidenav/sidenav.component';
 import { TopnavComponent } from './shared/topnav/topnav.component';
+import { LMarkdownEditorModule } from 'ngx-markdown-editor';
 
 import { ProjectOverviewComponent } from './feature/project-overview/project-overview.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, SidenavComponent, TopnavComponent, ProjectOverviewComponent],
+  imports: [RouterOutlet, SidenavComponent, TopnavComponent,FormsModule, LMarkdownEditorModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,15 @@
 import { Routes } from '@angular/router';
 import { ProjectOverviewComponent } from './feature/project-overview/project-overview.component';
+import { ProjectCreateComponent } from './feature/project-create/project-create.component';
 export const routes: Routes = [
 {
     path: 'projects',
     component: ProjectOverviewComponent
+},
+{
+    path:'projects/create',
+    component: ProjectCreateComponent
 }
-
-
 
 
 ];
