ca211c74424d1cd1d2ff8565ba73bfb1b3171e05
Lucaswillaert <lucas.willaert@student.ehb.be>
2025-02-02 11:23:56 +0100

    add project details components and routing: added project overview page added project feedback page added project members page added applications page more in the future: discussions (teachers) and management tools (github?jira? trello? )

---
+++ src/app/feature/project-details/components/details-overview/details-overview.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-details-overview',
+  imports: [],
+  templateUrl: './details-overview.component.html',
+  styleUrl: './details-overview.component.scss'
+})
+export class DetailsOverviewComponent {
+
+}
---
+++ src/app/feature/project-details/project-details.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-project-details',
+  imports: [CommonModule,RouterModule],
+  templateUrl: './project-details.component.html',
+  styleUrl: './project-details.component.scss'
+})
+export class ProjectDetailsComponent {
+
+}
