612fb571e3201c774484023e1ca15a3792afc157
Lucaswillaert <lucas.willaert@student.ehb.be>
2025-02-03 14:48:51 +0100

    added the members page and a dropdown role component for later use

--- src/app/feature/project-details/components/members/members.component.ts
+++ src/app/feature/project-details/components/members/members.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { RoleDropdownComponent } from '../../../../shared/role-dropdown/role-dropdown.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-members',
-  imports: [],
+  imports: [RoleDropdownComponent, CommonModule],
   templateUrl: './members.component.html',
   styleUrl: './members.component.scss'
 })
 export class MembersComponent {
+  members = [
+    {first_name: 'John', last_name: 'Doe', role: 'Product Owner', joinedOn: '22/01/2025', image: 'icons/placeholderpic.svg'},
+    {first_name: 'Jane', last_name: 'Doe', role: 'Developer', joinedOn: '22/01/2025', image: 'icons/placeholderpic.svg'},
+    {first_name: 'thomas', last_name:' the tank engine', role: 'Scrum Master', joinedOn: '22/01/2025', image: 'icons/placeholderpic.svg'},
+    {first_name: 'john', last_name: 'Cena', role: 'Developer', joinedOn: '22/01/2025', image: 'icons/placeholderpic.svg'}
+
+  ]
+
+  updateRole(index: number, newRole: string){
+    this.members[index].role = newRole;
+  }
+  removeMember(index: number){
+    this.members.splice(index, 1);
+  }
 
 }
