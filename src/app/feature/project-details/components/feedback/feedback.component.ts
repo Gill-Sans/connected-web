aa9589960def5e74e756ae366891d0f8f8cbdcdb
Lucaswillaert <lucas.willaert@student.ehb.be>
2025-02-03 12:27:54 +0100

    implement feedback component with submission functionality and conversation cards

--- src/app/feature/project-details/components/feedback/feedback.component.ts
+++ src/app/feature/project-details/components/feedback/feedback.component.ts
@@ -1,11 +1,44 @@
 import { Component } from '@angular/core';
+import { ConversationcardComponent } from "../../../../shared/conversationcard/conversationcard.component";
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { ButtonComponent } from '../../../../shared/button/button.component';
 
 @Component({
   selector: 'app-feedback',
-  imports: [],
+  imports: [ConversationcardComponent, CommonModule, FormsModule, ButtonComponent],
   templateUrl: './feedback.component.html',
   styleUrl: './feedback.component.scss'
 })
 export class FeedbackComponent {
+  feedbackList: { fullname: string; timestamp: string; message: string }[] =[
+    {
+      fullname: "Robin Bervoets",
+      timestamp: "10:16 21/01/2025",
+      message: "Het ingediende projectvoorstel en de wireframes bieden een goed uitgangspunt..."
+    },
+    {
+      fullname: "Jeroen Van den Berg",
+      timestamp: "09:45 21/01/2025",
+      message: "Ik ben van mening dat de wireframes niet voldoende zijn uitgewerkt..."
+    }
+  ]
+
+  //voor nieuwe feedback
+  newFeedback: string = '';
+
+  submitFeedback() {
+    if (this.newFeedback.trim() === '') return;
+
+    const now = new Date();
+    const timestamp = now.toLocaleTimeString() + " " + now.toLocaleDateString();
+
+    this.feedbackList.push({
+      fullname: "Current User",
+      timestamp: timestamp,
+      message: this.newFeedback
+    });
+    this.newFeedback = ''; // Input veld leegmaken
+  }
 
 }
---
+++ src/app/shared/conversationcard/conversationcard.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ConversationcardComponent } from './conversationcard.component';
+
+describe('ConversationcardComponent', () => {
+  let component: ConversationcardComponent;
+  let fixture: ComponentFixture<ConversationcardComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ConversationcardComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ConversationcardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
