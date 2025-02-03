26dc7d3c2844a8fbf28d008d48ec916d5f271b80
Lucaswillaert <lucas.willaert@student.ehb.be>
2025-02-03 15:07:13 +0100

    add status card component with styling and basic functionality

---
+++ src/app/shared/statuscard/statuscard.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-statuscard',
+  imports: [CommonModule],
+  templateUrl: './statuscard.component.html',
+  styleUrl: './statuscard.component.scss'
+})
+export class StatuscardComponent {
+  @Input() statusLabel: string= '';
+  @Input() type: 'Pending' | 'Approved' | 'Rejected' = 'Pending';
+}
