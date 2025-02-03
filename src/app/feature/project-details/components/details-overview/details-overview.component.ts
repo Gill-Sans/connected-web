90aff85720108a8d952862093200dc2a6f14fb9d
Lucaswillaert <lucas.willaert@student.ehb.be>
2025-02-03 21:33:10 +0100

    implemented more dummy data instead of hardcoded

--- src/app/feature/project-details/components/details-overview/details-overview.component.ts
+++ src/app/feature/project-details/components/details-overview/details-overview.component.ts
@@ -10,6 +10,18 @@ import { CommonModule } from '@angular/common';
   styleUrl: './details-overview.component.scss'
 })
 export class DetailsOverviewComponent {
+
+  project = {
+    title: 'ProjectConnect',
+    description: `Doel van de applicatie: ProjectConnect heeft als doel projectvoorstellen binnen te krijgen 
+        voor studiegerelateerde projecten met deadlines. De applicatie biedt een intuïtieve interface 
+        waarmee docenten deze voorstellen kunnen beoordelen en goed- of afkeuren. Studenten krijgen 
+        de mogelijkheid om te solliciteren voor de projecten die hen interesseren. Bovendien bevat 
+        ProjectConnect een module waarmee project pitchers hun teamleden kunnen 'aanwerven'.`,
+
+  };
+
+
   members = [
     {first_name : 'John', last_name: 'Doe', image: 'icons/placeholderpic.svg'},
     {first_name : 'Jane', last_name: 'Doe', image: 'icons/placeholderpic.svg'},
