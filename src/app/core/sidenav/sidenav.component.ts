72c6ca86ccea4b9fc4a994585126932689de4667
Gill Mertens <gill.mertens@student.ehb.be>
2025-02-05 17:05:02 +0100

    Fix: some styling

--- src/app/core/sidenav/sidenav.component.ts
+++ src/app/core/sidenav/sidenav.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { Role } from '../../auth/models/role.model';
+import {HasRoleDirective} from '../../shared/directives/HasRole.directive';
 
 
 @Component({
   selector: 'app-sidenav',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [
+    RouterLink,
+    RouterLinkActive,
+    HasRoleDirective
+  ],
   templateUrl: './sidenav.component.html',
   styleUrl: './sidenav.component.scss'
 })
