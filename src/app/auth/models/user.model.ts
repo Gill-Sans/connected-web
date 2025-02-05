4ac01f0820a1ea0c7f0f92e64279c7d7ef4c398d
Gill Mertens <gill.mertens@student.ehb.be>
2025-02-05 14:05:32 +0100

    Fix: Change Role from String to enum

--- src/app/auth/models/user.model.ts
+++ src/app/auth/models/user.model.ts
@@ -1,8 +1,10 @@
+import {Role} from './role.model';
+
 export interface User {
     id: Number,
     firstName: String,
     lastName: String,
     email: String,
     avatarUrl: String,
-    role: String,
+    role: Role,
 }
