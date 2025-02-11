d8a2a7b91bc6d3a306e620ed08fe90c40aca8552
Gill Mertens <gill.mertens@student.ehb.be>
2025-02-11 18:04:17 +0100

    Fix: display courses and assignments ins "space" dropdown

--- src/app/core/topnav/topnav.component.ts
+++ src/app/core/topnav/topnav.component.ts
@@ -1,11 +1,14 @@
 import {AuthFacade} from '../../auth/store/auth.facade';
 import {CommonModule} from '@angular/common';
-import {inject, Component} from '@angular/core';
+import {inject, Component, OnInit} from '@angular/core';
 import { Router } from '@angular/router';
 import { Role } from '../../auth/models/role.model';
 import {ClickOutsideDirective} from '../../shared/directives/click-outside.directive';
 import {Observable} from 'rxjs';
 import {User} from '../../auth/models/user.model';
+import {CourseService} from '../services/course.service';
+import {AssignmentService} from '../services/assignment.service';
+import {Course} from '../../shared/models/course.model';
 
 @Component({
   selector: 'app-topnav',
@@ -16,7 +19,7 @@ import {User} from '../../auth/models/user.model';
   templateUrl: './topnav.component.html',
   styleUrl: './topnav.component.scss'
 })
-export class TopnavComponent {
+export class TopnavComponent implements OnInit {
   public Role: typeof Role = Role;
   isHiddenAssignments: boolean = true;
   isHiddenProfile: boolean = true;
@@ -24,6 +27,17 @@ export class TopnavComponent {
   private readonly authFacade: AuthFacade = inject(AuthFacade);
   readonly user$:Observable<User | null> = this.authFacade.user$;
   private readonly router: Router = inject(Router);
+  private readonly courseService: CourseService = inject(CourseService);
+  private readonly assignmentService: AssignmentService = inject(AssignmentService);
+
+  courses$:Observable<Course[] | null> = this.courseService.getAllCourses();
+
+  ngOnInit(): void {
+    this.courses$.subscribe(courses => {
+
+    });
+  }
+
 
   toggleAssignmentsHidden() {
     this.isHiddenAssignments = !this.isHiddenAssignments;
