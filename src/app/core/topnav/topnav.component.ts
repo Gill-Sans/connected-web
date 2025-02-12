d478c914025b43ac50424fc513356d7d3b78febd
Gill Mertens <gill.mertens@student.ehb.be>
2025-02-12 10:03:30 +0100

    Feature: implement selection of current assignment

--- src/app/core/topnav/topnav.component.ts
+++ src/app/core/topnav/topnav.component.ts
@@ -1,66 +1,65 @@
 import {AuthFacade} from '../../auth/store/auth.facade';
 import {CommonModule} from '@angular/common';
-import {inject, Component, OnInit} from '@angular/core';
-import { Router } from '@angular/router';
-import { Role } from '../../auth/models/role.model';
+import {Component, inject, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import {Role} from '../../auth/models/role.model';
 import {ClickOutsideDirective} from '../../shared/directives/click-outside.directive';
 import {Observable} from 'rxjs';
 import {User} from '../../auth/models/user.model';
 import {CourseService} from '../services/course.service';
-import {AssignmentService} from '../services/assignment.service';
 import {Course} from '../../shared/models/course.model';
+import {ActiveAssignment, ActiveAssignmentService} from '../services/active-assignment.service';
+import {Assignment} from '../../shared/models/assignment.model';
 
 @Component({
-  selector: 'app-topnav',
-  imports: [
-    CommonModule,
-    ClickOutsideDirective
-  ],
-  templateUrl: './topnav.component.html',
-  styleUrl: './topnav.component.scss'
+    selector: 'app-topnav',
+    imports: [
+        CommonModule,
+        ClickOutsideDirective
+    ],
+    templateUrl: './topnav.component.html',
+    styleUrl: './topnav.component.scss'
 })
-export class TopnavComponent implements OnInit {
-  public Role: typeof Role = Role;
-  isHiddenAssignments: boolean = true;
-  isHiddenProfile: boolean = true;
+export class TopnavComponent {
+    public Role: typeof Role = Role;
+    isHiddenAssignments: boolean = true;
+    isHiddenProfile: boolean = true;
 
-  private readonly authFacade: AuthFacade = inject(AuthFacade);
-  readonly user$:Observable<User | null> = this.authFacade.user$;
-  private readonly router: Router = inject(Router);
-  private readonly courseService: CourseService = inject(CourseService);
-  private readonly assignmentService: AssignmentService = inject(AssignmentService);
+    private readonly authFacade: AuthFacade = inject(AuthFacade);
+    readonly user$: Observable<User | null> = this.authFacade.user$;
+    private readonly router: Router = inject(Router);
+    private readonly courseService: CourseService = inject(CourseService);
+    courses$: Observable<Course[] | null> = this.courseService.getAllCourses();
+    private readonly activeAssignmentService: ActiveAssignmentService = inject(ActiveAssignmentService);
+    public activeAssignment$: Observable<ActiveAssignment | null> = this.activeAssignmentService.activeAssignment$;
 
-  courses$:Observable<Course[] | null> = this.courseService.getAllCourses();
+    toggleAssignmentsHidden() {
+        this.isHiddenAssignments = !this.isHiddenAssignments;
+    }
 
-  ngOnInit(): void {
-    this.courses$.subscribe(courses => {
+    closeAssignmentsDropdown() {
+        this.isHiddenAssignments = true;
+    }
 
-    });
-  }
+    toggleHiddenprofile() {
+        this.isHiddenProfile = !this.isHiddenProfile;
+    }
 
+    // This method is called when a click is detected outside the dropdown.
+    closeProfileDropdown() {
+        this.isHiddenProfile = true;
+    }
 
-  toggleAssignmentsHidden() {
-    this.isHiddenAssignments = !this.isHiddenAssignments;
-  }
+    navigateToProfile() {
+        this.router.navigate(['/profile']);
+    }
 
-  closeAssignmentsDropdown() {
-    this.isHiddenAssignments = true;
-  }
+    logout() {
+        console.log('logout!');
+    }
 
-  toggleHiddenprofile() {
-    this.isHiddenProfile = !this.isHiddenProfile;
-  }
-
-  // This method is called when a click is detected outside the dropdown.
-  closeProfileDropdown() {
-    this.isHiddenProfile = true;
-  }
-
-  navigateToProfile() {
-    this.router.navigate(['/profile']);
-  }
-
-  logout() {
-    console.log('logout!');
-  }
+    selectAssignment(assignment: Assignment, course: Course): void {
+        this.activeAssignmentService.setActiveAssignment({ assignment, course });
+        this.isHiddenAssignments = true;
+    }
 }
