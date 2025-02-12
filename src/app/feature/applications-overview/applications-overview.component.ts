8433cccf481f2611f62dff124739c1af218441b5
Lucaswillaert <lucas.willaert@student.ehb.be>
2025-02-12 11:36:42 +0100

    Update applications overview to use async data and improve data display

--- src/app/feature/applications-overview/applications-overview.component.ts
+++ src/app/feature/applications-overview/applications-overview.component.ts
@@ -1,7 +1,13 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StatuscardComponent } from '../../shared/components/statuscard/statuscard.component';
-import { Router } from '@angular/router';
+import { Router,ActivatedRoute  } from '@angular/router';
+import { AssignmentService } from '../../core/services/assignment.service';
+import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject';
+import { Application } from '../../shared/models/application.model';
+import { Observable } from 'rxjs/internal/Observable';
+import { User } from '../../auth/models/user.model';
+import { ApplicationStatusEnum } from '../../shared/models/ApplicationStatus.enum';
 
 @Component({
   selector: 'app-applications-overview',
@@ -11,6 +17,18 @@ import { Router } from '@angular/router';
 })
 export class ApplicationsOverviewComponent {
   private router = inject(Router);
+  private route = inject(ActivatedRoute);
+  private assignmentService = inject(AssignmentService);
+  applications$: Observable<Application[]> | null = null;
+
+  ngOnInit() {
+     if (!this.route.snapshot.params['id']) {
+      console.error("No assignment id provided!");
+      return;
+     }
+     this.applications$ = this.assignmentService.getAllApplicationsFromAssignment(this.route.snapshot.params['id']);
+  }
+  
   applications = [{
     first_name: "Steve",last_name:"van der Beek" , project: "ConnectEd", status: "Pending", image: "icons/placeholderpic.svg"
   },
@@ -22,10 +40,14 @@ export class ApplicationsOverviewComponent {
   }
 ]
 
+  
+  
+
 navigateToApplication(application: any) {
   // Vervang '/application' met je gewenste route
   this.router.navigate(['/application', application.id]);
   // Voor nu loggen we alleen
   console.log('Navigating to application:', application);
+
 }
 }
--- src/app/feature/project-details/components/applications/applications.component.ts
+++ src/app/feature/project-details/components/applications/applications.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { StatuscardComponent } from '../../../../shared/components/statuscard/statuscard.component';
 import { CommonModule } from '@angular/common';
+import { ApplicationStatusEnum } from '../../../../shared/models/ApplicationStatus.enum';
 
 @Component({
   selector: 'app-applications',
@@ -11,8 +12,8 @@ import { CommonModule } from '@angular/common';
 export class ApplicationsComponent {
 
   applications = [
-    { first_name: "Steven", role: "Developer", status: "Pending", image: "icons/placeholderpic.svg" },
-    { first_name: "Wim", role: "Scrum Master", status: "Approved", image: "icons/placeholderpic.svg" },
-    { first_name: "Lucas", role: "Developer", status: "Rejected", image: "icons/placeholderpic.svg" }
+    { first_name: "Steven", role: "Developer", status: ApplicationStatusEnum.PENDING, image: "icons/placeholderpic.svg" },
+    { first_name: "Wim", role: "Scrum Master", status: ApplicationStatusEnum.ACCEPTED, image: "icons/placeholderpic.svg" },
+    { first_name: "Lucas", role: "Developer", status: ApplicationStatusEnum.REJECTED, image: "icons/placeholderpic.svg" }
   ];
 }
