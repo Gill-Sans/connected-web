2a41bbc2caac33a166fd2afc5a56aada07cd9083
BryanLanghendries <bryan.langhendries@student.ehb.be>
2025-02-14 15:55:49 +0100

    feat: add feedback model and integrate feedback functionality in project details

--- src/app/auth/models/user.model.ts
+++ src/app/auth/models/user.model.ts
@@ -1,11 +1,11 @@
-import {Role} from './role.model';
+import { Role } from './role.model';
 import { tag } from '../../shared/models/tag.model';
 import { Application } from '../../shared/models/application.model';
 
 export interface User {
     id: number,
-    firstName?: string,
-    lastName?: string,
+    firstName: string,
+    lastName: string,
     email: string,
     avatarUrl?: string,
     role: Role,
