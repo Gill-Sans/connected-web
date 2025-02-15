6a1f9341d51bc43b480d6f46d596becd10e568d4
BryanLanghendries <bryan.langhendries@student.ehb.be>
2025-02-15 17:54:26 +0100

    feat: update status handling in applications and members components for improved type consistency

--- src/app/feature/project-details/components/members/members.component.ts
+++ src/app/feature/project-details/components/members/members.component.ts
@@ -1,66 +1,30 @@
-import {Component} from '@angular/core';
-import {RoleDropdownComponent} from '../../../../shared/components/role-dropdown/role-dropdown.component';
+import {Component, inject, OnInit} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {ConfirmDialogComponent} from '../../../../shared/components/confirm-dialog/confirm-dialog.component';
+import {Observable} from 'rxjs';
+import {Project} from '../../../../shared/models/project.model';
+import {ProjectService} from '../../../../core/services/project.service';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
     selector: 'app-members',
-    imports: [RoleDropdownComponent, CommonModule, ConfirmDialogComponent],
+    imports: [CommonModule],
     templateUrl: './members.component.html',
     styleUrl: './members.component.scss'
 })
-export class MembersComponent {
-    members = [
-        {
-            first_name: 'John',
-            last_name: 'Doe',
-            role: 'Product Owner',
-            joinedOn: '22/01/2025',
-            image: 'icons/placeholderpic.svg'
-        },
-        {
-            first_name: 'Jane',
-            last_name: 'Doe',
-            role: 'Developer',
-            joinedOn: '22/01/2025',
-            image: 'icons/placeholderpic.svg'
-        },
-        {
-            first_name: 'thomas',
-            last_name: ' the tank engine',
-            role: 'Scrum Master',
-            joinedOn: '22/01/2025',
-            image: 'icons/placeholderpic.svg'
-        },
-        {
-            first_name: 'john',
-            last_name: 'Cena',
-            role: 'Developer',
-            joinedOn: '22/01/2025',
-            image: 'icons/placeholderpic.svg'
-        }
+export class MembersComponent implements OnInit {
+    private readonly projectService: ProjectService = inject(ProjectService);
+    private readonly route: ActivatedRoute = inject(ActivatedRoute);
+    private projectId: string = 'undefined';
+    public project$: Observable<Project> | null = null;
 
-    ]
-
-    memberToDelete: number | null = null; // Houdt bij wie verwijderd wordt
-
-    updateRole(index: number, newRole: string) {
-        this.members[index].role = newRole;
-    }
-
-    confirmRemoveMember(index: number) {
-        this.memberToDelete = index; // Zet de member die verwijderd moet worden
-    }
-
-    removeMember() {
-        if (this.memberToDelete !== null) {
-            this.members.splice(this.memberToDelete, 1);
-            this.memberToDelete = null; // Reset waarde na verwijderen
-        }
-    }
-
-    cancelRemove() {
-        this.memberToDelete = null;
+    ngOnInit() {
+        this.route.parent?.params.subscribe(params => {
+            const id = params['id'];
+            if (id) {
+                this.projectId = id;
+                this.project$ = this.projectService.getProject(this.projectId);
+            }
+        });
     }
 
 }
