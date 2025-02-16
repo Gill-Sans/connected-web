595fe6bad63752c514c4dc449731365907d1b7b4
BryanLanghendries <bryan.langhendries@student.ehb.be>
2025-02-16 14:54:22 +0100

    feat: enhance application details with clickable project title for navigation

--- src/app/feature/applications-overview/application-details/application-details.component.ts
+++ src/app/feature/applications-overview/application-details/application-details.component.ts
@@ -1,25 +1,39 @@
-import { Component, inject } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import { ApplicationService } from '../../../core/services/application.service';
-import { ActivatedRoute } from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from '../../../shared/components/button/button.component';
 import { MarkdownModule } from 'ngx-markdown';
+import {Application} from 'express';
+import {ActiveAssignmentRoutingService} from '../../../core/services/active-assignment-routing.service';
 @Component({
   selector: 'app-application-details',
   imports: [CommonModule,ButtonComponent,MarkdownModule],
   templateUrl: './application-details.component.html',
   styleUrl: './application-details.component.scss'
 })
-export class ApplicationDetailsComponent {
+
+export class ApplicationDetailsComponent implements OnInit {
   private readonly applicationService = inject(ApplicationService);
   private readonly route = inject(ActivatedRoute);
+  private router: Router = inject(Router);
+  private readonly activeAssignmentRouteService: ActiveAssignmentRoutingService = inject(ActiveAssignmentRoutingService);
   applicationId = this.route.snapshot.params['id'];
   application$ = this.applicationService.getApplication(this.applicationId);
+  projectId: number | null = null;
 
   ngOnInit() {
     this.application$.subscribe(application => {
       console.log('Application:', application);
+      this.projectId = application.project.id;
     });
   }
 
+    navigateToProject() {
+      if (this.projectId === null) {
+        console.error('Project ID is not set');
+        return;
+      }
+        this.router.navigate(this.activeAssignmentRouteService.buildRoute('projects', this.projectId.toString()));
+    }
 }
