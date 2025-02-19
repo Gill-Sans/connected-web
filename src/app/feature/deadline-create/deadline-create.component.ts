1b76cf06bd3402e45776aef701d344b22954c822
BryanLanghendries <bryan.langhendries@student.ehb.be>
2025-02-19 20:39:42 +0100

    feat: enhance deadline overview styling and structure for better readability

---
+++ src/app/feature/deadline-create/deadline-create.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-deadline-create',
+  imports: [],
+  templateUrl: './deadline-create.component.html',
+  styleUrl: './deadline-create.component.scss'
+})
+export class DeadlineCreateComponent {
+
+}
