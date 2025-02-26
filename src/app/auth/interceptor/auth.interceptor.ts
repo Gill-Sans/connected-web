08ba45c1c58bcd9c94546d672e321f132eec073f
BryanLanghendries <bryan.langhendries@student.ehb.be>
2025-02-26 12:21:16 +0100

    feat: enhance auth interceptor to use ToastService for error notifications

--- src/app/auth/interceptor/auth.interceptor.ts
+++ src/app/auth/interceptor/auth.interceptor.ts
@@ -7,26 +7,28 @@ import {
 import { Observable, throwError } from 'rxjs';
 import {catchError, tap} from 'rxjs/operators';
 import { AuthFacade } from '../store/auth.facade';
+import {ToastService} from '../../core/services/toast.service';
 
 export function authInterceptor(
     req: HttpRequest<unknown>,
     next: HttpHandlerFn
 ): Observable<HttpEvent<unknown>> {
     const authFacade: AuthFacade = inject(AuthFacade);
+    const toastService: ToastService = inject(ToastService);
 
     return next(req).pipe(
         tap(event => {
-            // Only log the successful response
             if (event instanceof HttpResponse) {
             }
         }),
         catchError((error: HttpErrorResponse) => {
             if (error.status == 401) {
-                console.log('401 error, redirecting to login');
+                toastService.showToast("error", 'Session expired, please login again');
                 document.cookie = `JSESSIONID=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
                 authFacade.redirectToLogin();
+            } else {
+                toastService.showToast("error", 'Something went wrong, please try again');
             }
-            // Propagate the error so that any subscribers can handle it
             return throwError(() => error);
         })
     );
