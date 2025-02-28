bbf3f0de18c6132639d729f7c48a9917932344a1
BryanLanghendries <bryan.langhendries@student.ehb.be>
2025-02-28 15:03:36 +0100

    feat: enhance calendar component with custom button styling and improved event display

--- src/app/shared/components/calendar/calendar.component.ts
+++ src/app/shared/components/calendar/calendar.component.ts
@@ -1,22 +1,33 @@
 import {Component, OnInit, ViewEncapsulation} from '@angular/core';
-import { CommonModule,registerLocaleData } from '@angular/common';
+import {CommonModule, registerLocaleData} from '@angular/common';
 import localeNl from '@angular/common/locales/nl';
-import {CalendarEvent, CalendarModule, CalendarView, CalendarUtils, DateAdapter, CalendarDateFormatter, CalendarA11y, CalendarEventTitleFormatter} from 'angular-calendar';
-import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+import {
+    CalendarEvent,
+    CalendarModule,
+    CalendarView,
+    CalendarUtils,
+    DateAdapter,
+    CalendarDateFormatter,
+    CalendarA11y,
+    CalendarEventTitleFormatter
+} from 'angular-calendar';
+import {adapterFactory} from 'angular-calendar/date-adapters/date-fns';
 import {DeadlineService} from '../../../core/services/deadline.service';
-import { addMonths, startOfToday, subMonths } from 'date-fns';
+import {addMonths, startOfToday, subMonths} from 'date-fns';
 import {Deadline} from '../../models/deadline.model';
 import {ActiveAssignmentService} from '../../../core/services/active-assignment.service';
+import {ButtonComponent} from '../button/button.component';
 
 registerLocaleData(localeNl);
 
+
 @Component({
-  selector: 'app-calendar',
-  imports: [ CommonModule, CalendarModule ],
-    providers: [ { provide: DateAdapter, useFactory: adapterFactory,deps: [] }, CalendarUtils,  CalendarDateFormatter,
-        CalendarA11y,CalendarEventTitleFormatter ],
-  templateUrl: './calendar.component.html',
-  styleUrl: './calendar.component.scss',
+    selector: 'app-calendar',
+    imports: [CommonModule, CalendarModule, ButtonComponent],
+    providers: [{provide: DateAdapter, useFactory: adapterFactory, deps: []}, CalendarUtils, CalendarDateFormatter,
+        CalendarA11y, CalendarEventTitleFormatter],
+    templateUrl: './calendar.component.html',
+    styleUrls: ['node_modules/angular-calendar/dist/css/angular-calendar.css', './calendar.component.scss'],
     encapsulation: ViewEncapsulation.None
 })
 export class CalendarComponent implements OnInit {
