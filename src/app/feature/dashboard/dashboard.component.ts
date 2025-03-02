3b67ed114c1f9f38ffa0f56f04108b31c9fc00b6
Gill Mertens <gill.mertens@student.ehb.be>
2025-03-02 17:16:25 +0100

    feat: add project container to dashboard with project card and navigation

--- src/app/feature/dashboard/dashboard.component.ts
+++ src/app/feature/dashboard/dashboard.component.ts
@@ -4,53 +4,79 @@ import {ActiveAssignmentService} from '../../core/services/active-assignment.ser
 import {ActiveAssignmentRoutingService} from '../../core/services/active-assignment-routing.service';
 import {Router} from '@angular/router';
 import {Announcement} from '../../shared/models/announcement.model';
-import {Subscription} from 'rxjs';
+import {Observable, Subscription} from 'rxjs';
 import {CommonModule} from '@angular/common';
 import {AnnouncementCardComponent} from '../../shared/components/announcement-card/announcement-card.component';
 import {ActiveAssignment} from '../../shared/models/activeAssignment.model';
 import {CalendarComponent} from '../../shared/components/calendar/calendar.component';
+import {ButtonComponent} from '../../shared/components/button/button.component';
+import {ProjectService} from '../../core/services/project.service';
+import {Project} from '../../shared/models/project.model';
+import {ProjectcardComponent} from '../../shared/components/projectcard/projectcard.component';
 
 @Component({
   selector: 'app-dashboard',
   imports: [
       CommonModule,
       AnnouncementCardComponent,
-      CalendarComponent
-
+      CalendarComponent,
+      ButtonComponent,
+      ProjectcardComponent
   ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent implements OnInit, OnDestroy {
     private announcementService: AnnouncementService = inject(AnnouncementService);
+    private readonly projectService: ProjectService = inject(ProjectService);
     private activeAssignmentService: ActiveAssignmentService = inject(ActiveAssignmentService);
     private activeAssignmentRoutingService: ActiveAssignmentRoutingService = inject(ActiveAssignmentRoutingService);
     private router: Router = inject(Router);
 
     announcements: Announcement[] = [];
-    activeAssignment: ActiveAssignment | null = null;
+    activeAssignment$: Observable<ActiveAssignment | null> = this.activeAssignmentService.activeAssignment$;
     private subscriptions: Subscription[] = [];
+    project: Project | null = null;
 
     ngOnInit(): void {
-        console.log("DashboardComponent ngOnInit");
-        this.activeAssignment = this.activeAssignmentService.getActiveAssignment();
-        if (this.activeAssignment) {
-            this.subscriptions.push(this.announcementService.getAnnouncements(this.activeAssignment.assignment.id)
-                .subscribe({
-                    next: (announcements) => {
-                        console.log("Announcements loaded", announcements);
-                        this.announcements = announcements;
-                    },
-                    error: (err) => {
-                        console.error("Failed to load announcements", err);
-                    }
-                })
-            );
-        }
+        this.subscriptions.push(this.activeAssignment$.subscribe({
+            next: (activeAssignment) => {
+                console.log("Active assignment changed", activeAssignment);
+                if (activeAssignment) {
+                    this.subscriptions.push(this.announcementService.getAnnouncements(activeAssignment.assignment.id)
+                        .subscribe({
+                            next: (announcements) => {
+                                console.log("Announcements loaded", announcements);
+                                this.announcements = announcements;
+                            },
+                            error: (err) => {
+                                console.error("Failed to load announcements", err);
+                            }
+                        })
+                    );
+
+                    this.subscriptions.push(this.projectService.getProjectByUserAndAssignmentId(activeAssignment.assignment.id.toString())
+                        .subscribe({
+                            next: (project) => {
+                                console.log("Project loaded", project);
+                                this.project = project;
+                            }
+                        })
+                    );
+                }
+            },
+            error: (err) => {
+                console.error("Failed to load active assignment", err);
+            }
+        }));
     }
 
-    createAnnouncement(): void {
-        this.router.navigate(this.activeAssignmentRoutingService.buildRoute('announcements', 'create'));
+    navigateToProjects(): void {
+        this.router.navigate(this.activeAssignmentRoutingService.buildRoute('projects'));
+    }
+
+    navigateToProject(id: number): void {
+        this.router.navigate(this.activeAssignmentRoutingService.buildRoute('project', id.toString()));
     }
 
     ngOnDestroy(): void {
