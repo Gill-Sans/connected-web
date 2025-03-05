e6b2fb7120a349c91b63495bbd8e0e478bac38f1
Gill Mertens <gill.mertens@student.ehb.be>
2025-03-05 20:56:24 +0100

    feat: add GDPR consent checkbox to login component and improve styling

--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -3,6 +3,7 @@ import {CommonModule} from '@angular/common';
 import {AuthFacade} from '../../store/auth.facade';
 import {ButtonComponent} from '../../../shared/components/button/button.component';
 import {FormsModule} from '@angular/forms';
+import {Router} from '@angular/router';
 
 @Component({
     selector: 'app-login',
@@ -10,7 +11,6 @@ import {FormsModule} from '@angular/forms';
         CommonModule,
         ButtonComponent,
         FormsModule,
-
     ],
     templateUrl: './login.component.html',
     styleUrls: ['./login.component.scss'],
@@ -19,9 +19,25 @@ import {FormsModule} from '@angular/forms';
 export class LoginComponent {
     @Input() isLoginVisible = true;
     private readonly authFacade = inject(AuthFacade);
+    readonly router = inject(Router);
+
+    // Bind dit aan de checkbox
+    accepted: boolean = false;
+    error: string = '';
 
     loginWithCanvas(): void {
-        // Dispatch the action to redirect to the OAuth2 authorization endpoint.
+        if (!this.accepted) {
+            this.error = "Please read the data guides carefully and check this box before continuing authentication.";
+            return;
+        }
+        // Clear error and proceed
+        this.error = "";
         this.authFacade.redirectToCanvasLogin();
+    }
+
+    onCheckboxChange(value: boolean): void {
+        if (value) {
+            this.error = "";
+        }
     }
 }
